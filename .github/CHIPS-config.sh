06eb87745b8351911d912f411c98905822686196
Evan Sum <evansum@Evans-Macbook.local>
2021-03-10 18:32:23 -0800

    added some secrets

--- .github/CHIPS-config.sh
+++ .github/CHIPS-config.sh
@@ -5,4 +5,4 @@ ci_repo_link="https://github.com/evan1997123/hw-ruby-intro-ci"
 not_ci_repo_ssh="git@github.com:evan1997123/hw-ruby-intro.git"
 commit_message="Automated Commit. CHIPS ${chips_number}: ${chips_name} - This repository is created by automation from \
 github workflows, found at ${ci_repo_link}." 
-final_folder="./hw-ruby-intro"
+final_folder="./hw-ruby-intro" 
