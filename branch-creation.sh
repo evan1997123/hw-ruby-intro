0efe6906d64bbf628d5ee69352db80f01c163964
Evan Sum <evansum@Evans-Macbook.local>
2021-03-10 20:37:20 -0800

    Squashed '.github/workflows/' changes from e6db4b8..77bc7fc
    77bc7fc no more deep copy, checking starter_code
    
    git-subtree-dir: .github/workflows
    git-subtree-split: 77bc7fcb4e307e330758f73bd8be2099c4b82cb1

--- branch-creation.sh
+++ branch-creation.sh
@@ -1,73 +1,21 @@
-# #!/bin/bash
-
-# source ./.github/CHIPS-config.sh
-
-# git branch -a
-# if [[ `git branch -a 2>&1` = *develop-starter-code* ]]; then
-#     echo "no need to create a new branch"
-# else
-#     echo "need to create new branch"
-#     git branch develop-starter-code
-# fi
-
-# git checkout develop-starter-code
-
-# python3 ./.github/workflows/evaluate-hierarchy.py ./build_starter_code.json
-
-# #https://unix.stackexchange.com/questions/422392/delete-all-folders-inside-a-folder-except-one-with-specific-name
-# echo "LS BEFORE REMOVING"
-# ls -A
-
-# # echo "what is inside final_folder"
-# # ls ${final_folder} -A
-# # move .git into root folder
-# mv .git ./root
-
-# # remove everything by root folder
-# find . -mindepth 1 ! -regex "${final_folder}" -delete
-
-# # move everything up one directory
-# mv root/* root/.[!.]* . && rmdir root
-
-# git add -A
-
-# git -c user.name="GitHub Actions" -c user.email="actions@github.com" commit -m "${commit_message}" --author="$CURRENT_USER <$CURRENT_USER@users.noreply.github.com>"
-
-# git remote add not-ci ${not_ci_repo_ssh}
-
-# git fetch --unshallow not-ci
-
-# if [[ `git branch -r 2>&1` = *not-ci/develop-starter-code* ]]; then
-#     echo "pull develop-starter-code"
-#     PUSH_OPT=""
-# else
-#     echo "no need to pull develop-starter-code"
-#     PUSH_OPT="-u"
-# fi
-
-# git push -f ${PUSH_OPT} not-ci develop-starter-code
-
 #!/bin/bash
 
 source ./.github/CHIPS-config.sh
 
 git branch -a
-if [[ `git branch -a 2>&1` = *develop-codio* ]]; then
+if [[ `git branch -a 2>&1` = *develop-starter-code* ]]; then
     echo "no need to create a new branch"
 else
     echo "need to create new branch"
-    git branch develop-codio
+    git branch develop-starter-code
 fi
 
-git checkout develop-codio
+git checkout develop-starter-code
 
-echo "LS before anything"
-ls -A
-
-python3 ./.github/workflows/evaluate-hierarchy.py ./build_codio.json
+python3 ./.github/workflows/evaluate-hierarchy.py ./build_starter_code.json
 
 #https://unix.stackexchange.com/questions/422392/delete-all-folders-inside-a-folder-except-one-with-specific-name
-echo "LS AFTER script and BEFORE REMOVING"
+echo "LS BEFORE REMOVING"
 ls -A
 
 # echo "what is inside final_folder"
@@ -89,13 +37,65 @@ git remote add not-ci ${not_ci_repo_ssh}
 
 git fetch --unshallow not-ci
 
-if [[ `git branch -r 2>&1` = *not-ci/develop-codio* ]]; then
-    echo "pull develop-codio"
+if [[ `git branch -r 2>&1` = *not-ci/develop-starter-code* ]]; then
+    echo "pull develop-starter-code"
     PUSH_OPT=""
 else
-    echo "no need to pull develop-codio"
+    echo "no need to pull develop-starter-code"
     PUSH_OPT="-u"
 fi
 
-git push -f ${PUSH_OPT} not-ci develop-codio
+git push -f ${PUSH_OPT} not-ci develop-starter-code
+
+# #!/bin/bash
+
+# source ./.github/CHIPS-config.sh
+
+# git branch -a
+# if [[ `git branch -a 2>&1` = *develop-codio* ]]; then
+#     echo "no need to create a new branch"
+# else
+#     echo "need to create new branch"
+#     git branch develop-codio
+# fi
+
+# git checkout develop-codio
+
+# echo "LS before anything"
+# ls -A
+
+# python3 ./.github/workflows/evaluate-hierarchy.py ./build_codio.json
+
+# #https://unix.stackexchange.com/questions/422392/delete-all-folders-inside-a-folder-except-one-with-specific-name
+# echo "LS AFTER script and BEFORE REMOVING"
+# ls -A
+
+# # echo "what is inside final_folder"
+# # ls ${final_folder} -A
+# # move .git into root folder
+# mv .git ./root
+
+# # remove everything by root folder
+# find . -mindepth 1 ! -regex "${final_folder}" -delete
+
+# # move everything up one directory
+# mv root/* root/.[!.]* . && rmdir root
+
+# git add -A
+
+# git -c user.name="GitHub Actions" -c user.email="actions@github.com" commit -m "${commit_message}" --author="$CURRENT_USER <$CURRENT_USER@users.noreply.github.com>"
+
+# git remote add not-ci ${not_ci_repo_ssh}
+
+# git fetch --unshallow not-ci
+
+# if [[ `git branch -r 2>&1` = *not-ci/develop-codio* ]]; then
+#     echo "pull develop-codio"
+#     PUSH_OPT=""
+# else
+#     echo "no need to pull develop-codio"
+#     PUSH_OPT="-u"
+# fi
+
+# git push -f ${PUSH_OPT} not-ci develop-codio
 
